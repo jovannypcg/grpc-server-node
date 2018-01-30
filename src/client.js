e78ab262572a7b797ca34a1f6b7a3aebf129d9aa
Jovanny Cruz <jovannypcg@yahoo.com>
2018-01-30 17:00:20 -0600

    Add AMQP reader for client

--- src/client.js
+++ src/client.js
@@ -1,17 +1,69 @@
 'use strict';
 
 const debug = require('debug')('grpc-client');
+const readline = require('readline');
 
 const PORT = 5000;
 const HOST = 'localhost';
 const PROTO_PATH = __dirname + '/../proto/services.proto';
 
+const AMQP_TAG = " [AMQP *] ===> ";
+
 let grpc = require('grpc');
+let amqp = require('amqplib/callback_api');
+
 let services = grpc.load(PROTO_PATH).services;
 let messages = grpc.load(PROTO_PATH).messages;
 
+const AMQP_QUEUE = 'repository_enrollments';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function main() {
+  let exit = false;
+
+  rl.setPrompt('Operation? [S/r/e]: ');
+  rl.prompt();
+  console.log();
+
+  rl.on('line', function(line) {
+      if (line === "e") rl.close();
+
+      switch (line) {
+        case 'r':
+          read();
+          break;
+
+        default:
+          send();
+      }
+
+      rl.prompt();
+  }).on('close',function(){
+      process.exit(0);
+  });
+}
+
+function read() {
+  debug(AMQP_TAG + "Connecting to RabbitMQ...");
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      ch.assertQueue(AMQP_QUEUE, { durable: true });
+
+      debug("#{AMQP_TAG}Waiting for messages in #{AMQP_QUEUE}. To exit press CTRL+C");
+      ch.consume(AMQP_QUEUE, function(msg) {
+        debug(AMQP_TAG + "Received " + msg.content.toString);
+      }, {noAck: true});
+    });
+  });
+}
+
+function send() {
   let stub = new services.RepositoryEnrollerService((HOST + ':' + PORT), grpc.credentials.createInsecure());
+
   let repository = {
     name: "worker_ui",
     description: "Frontend application for the Worker UI.",
@@ -24,8 +76,7 @@ function main() {
     }
   };
 
-  debug("Stub: " + stub);
-  debug("Repository: " + repository);
+  debug("Sending repository with name: #{repository.name}...");
 
   stub.enroll(repository, function(err, response) {
     debug("Error: " + err);
